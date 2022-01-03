7d5f4ccc847e36dc66d686d1783751b8cb5ba65d
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2022-01-03 23:59:20 +0900

    backport

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftUIReplicator",
     platforms: [
-        .iOS(.v14),
+        .iOS(.v13),
         .macOS(.v11)
     ],
     products: [
